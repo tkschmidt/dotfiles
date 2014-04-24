17d36f848c9834cd261ccf67d21e5bd32ad7f4fd
tkschmidt <tobias@tobiass-air.fritz.box>
2014-04-24 13:27:43 +0200

    renaming bin, adding bin path to PATH, make executable

--- makesymlink.sh
+++ makesymlink.sh
@@ -17,16 +17,28 @@ files="vimrc zshrc zsh bin"    # list of files/folders to symlink in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
+echo ""
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
+echo ""
+
+# make shellscripts executable
+echo "Make files in $dir/bin executable"
+for file in $(ls $dir/bin/); do
+	exeFile=$dir/bin/$file
+	echo "make $exeFile executable"
+	chmod +x $exeFile
+done
+echo ""
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file/
+    ln -s $dir/$file ~/.$file
+    echo ""
 done
